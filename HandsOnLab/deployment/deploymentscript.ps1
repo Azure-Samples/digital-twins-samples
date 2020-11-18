06c757aa793fc74ec622e2aafca7560d8d2c70fa
teodelas <teodelas@microsoft.com>
2020-11-17 19:58:59 -0500

    ADT Hol

--- HandsOnLab/deployment/deploymentscript.ps1
+++ HandsOnLab/deployment/deploymentscript.ps1
@@ -41,6 +41,8 @@ $twinupdatefunctionname = $random + "-twinupdatefunction"
 
 az group create -n $rgname -l $location
 az dt create --dt-name $dtname -g $rgname -l $location
+Write-Host "Pausing for 60 seconds..."
+Start-Sleep -Seconds 60
 az dt role-assignment create -n $dtname -g $rgname --role "Azure Digital Twins Data Owner" --assignee $username -o json
 $adthostname = "https://" + $(az dt show -n $dtname --query 'hostName' -o tsv)
 
@@ -64,10 +66,12 @@ $relname = "rel_runs_lines"
 az dt twin relationship create -n $dtname --relationship $relname --twin-id "FactoryFloor" --target "ProductionLine" --relationship-id "Floor run production lines"
 $relname = "rel_runs_steps"
 az dt twin relationship create -n $dtname --relationship $relname --twin-id "ProductionLine" --target "GrindingStep" --relationship-id "Floor run production lines"
-
+Write-Host "Pausing for 60 seconds..."
+Start-Sleep -Seconds 60
 #Setup Azure Function
 az storage account create --name $functionstorage --location $location --resource-group $rgname --sku Standard_LRS
 az functionapp create --resource-group $rgname --consumption-plan-location $location --runtime dotnet --functions-version 3 --name $telemetryfunctionname --storage-account $functionstorage
+
 $principalID = $(az functionapp identity assign -g $rgname -n $telemetryfunctionname  --query principalId)
 az dt role-assignment create --dt-name $dtname --assignee $principalID --role "Azure Digital Twins Data Owner"
 az functionapp config appsettings set -g $rgname -n $telemetryfunctionname --settings "ADT_SERVICE_URL=$adthostname "
