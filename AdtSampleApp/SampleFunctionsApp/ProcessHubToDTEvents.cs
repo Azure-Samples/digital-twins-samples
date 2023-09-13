6588eaaec91e973a2425eb3614cd0a557f26f664
Bailey Anderson <baanders@microsoft.com>
2023-09-13 13:20:32 -0400

    Update logger

--- AdtSampleApp/SampleFunctionsApp/ProcessHubToDTEvents.cs
+++ AdtSampleApp/SampleFunctionsApp/ProcessHubToDTEvents.cs
@@ -20,8 +20,15 @@ namespace SampleFunctionsApp
         private static readonly HttpClient httpClient = new HttpClient();
         private static string adtServiceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
 
+        private readonly ILogger _logger;
+
+        public ProcessHubToDTEvents(ILogger<ProcessDTRoutedData> logger)
+        {
+            _logger = logger;
+        }
+
         [Function("ProcessHubToDTEvents")]
-        public async Task Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        public async Task Run([EventGridTrigger]EventGridEvent eventGridEvent)
         {
             // After this is deployed, you need to turn the Managed Identity Status to "On",
             // Grab Object Id of the function and assigned "Azure Digital Twins Owner (Preview)" role
@@ -32,18 +39,18 @@ namespace SampleFunctionsApp
             DigitalTwinsClient client = new DigitalTwinsClient(
                 new Uri(adtServiceUrl), credentials, new DigitalTwinsClientOptions
                 { Transport = new HttpClientTransport(httpClient) });
-            log.LogInformation($"ADT service client connection created.");
+            _logger.LogInformation($"ADT service client connection created.");
 
             if (eventGridEvent != null && eventGridEvent.Data != null)
             {
-                log.LogInformation(eventGridEvent.Data.ToString());
+                _logger.LogInformation(eventGridEvent.Data.ToString());
 
                 // Reading deviceId and temperature for IoT Hub JSON
                 JObject deviceMessage = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
                 string deviceId = (string)deviceMessage["systemProperties"]["iothub-connection-device-id"];
                 var temperature = deviceMessage["body"]["Temperature"];
 
-                log.LogInformation($"Device:{deviceId} Temperature is:{temperature}");
+                _logger.LogInformation($"Device:{deviceId} Temperature is:{temperature}");
 
                 //Update twin using device temperature
                 var updateTwinData = new JsonPatchDocument();
